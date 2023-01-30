eff118c87b5a738b0759b3a832aad95b8debbdef
c1294114 Gineude Silvestre de Carvalho <c1294114@interno.bb.com.br>
2023-01-30 07:44:34 -0300

    curso python - iniciando

---
+++ curso_python/aula1_comentarios.py
@@ -0,0 +1,15 @@
+# Comentário de uma linha
+print(123)
+
+# Docstring
+"""
+Não é um comentário,
+mas permite-se comentar
+com várias linhas
+"""
+'''
+Assim também permite
+explicar código ou fazer
+comentários com várias linhas
+'''
+print(456)
---
+++ curso_python/aula3_strings.py
@@ -0,0 +1,22 @@
+"""
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+
+# Aspas simples
+print('Gineude Silvestre')
+# Truque
+print('Gineude "Silvestre"')
+
+# Aspas duplas
+print("Gineude Silvestre")
+# Truque
+print("Gineude 'Silvestre'")
+
+# Escape
+print("Gineude \" Silvestre")
+
+# r
+print(r"Gineude \" Silvestre\"")
