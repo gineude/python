9f2b5b09713e2e44218c33992e882e64355be11a
c1294114 Gineude Silvestre de Carvalho <c1294114@interno.bb.com.br>
2023-01-31 05:46:17 -0300

    Aula 8 exercicio

---
+++ curso_python/aula8_exercicio.py
@@ -0,0 +1,13 @@
+nome = 'Gineude'
+sobrenome = 'Silvestre'
+idade = 42
+ano_nascimento = 1980
+maior_de_idade = ('Sim' if idade >= 18 else 'Não')
+altura_metros = 1.70
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura me metros:', altura_metros)
