0451ba93bbcb4bf7cb30a92521246eeae13962d4
c1294114 Gineude Silvestre de Carvalho <c1294114@interno.bb.com.br>
2023-01-31 05:34:15 -0300

    Aula 7 variaveis.

---
+++ curso_python/aula7_variaveis.py
@@ -0,0 +1,9 @@
+# Variáveis são usadas para salvar algo na memória do computador.
+# PEP8: inicie variáveis com letras minúsculas, pode usar
+# números e undeline _.
+# O sinal de = é o operador de atribuição. Ele é usado
+# para atribuir um valor a um nome (Variável).
+# Uso: nome_variavel = expressão
+nome_completo = 'Gineude Silvestre de Carvalho'
+soma_dois_mais_dois = 2 + 2
+print(nome_completo, soma_dois_mais_dois)
