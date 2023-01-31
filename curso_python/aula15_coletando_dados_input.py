2dccfbb798ff050d2b01b49bba85269c764712d9
c1294114 Gineude Silvestre de Carvalho <c1294114@interno.bb.com.br>
2023-01-31 15:25:07 -0300

    Aula 15 coletando dados do usuario com a funcao input()

---
+++ curso_python/aula15_coletando_dados_input.py
@@ -0,0 +1,5 @@
+# tudo que é digitado pelo o usuário é uma string
+# sendo que quando se quer um número é nescessário
+# realizar uma coersão para o tipo desejado
+nome = input('Qual é o seu nome? ')
+print('Bem vindo,', nome)
