89d061e47f8907b8d61b6603a3fa003cb539ab29
c1294114 Gineude Silvestre de Carvalho <c1294114@interno.bb.com.br>
2023-02-01 07:25:54 -0300

    Aula 10 na pasta errada

---
+++ curso_python/aula17_if_elif_else.py
@@ -0,0 +1,10 @@
+# if / elif      / else
+# se / se não se / se não
+condicao = True
+
+if condicao:
+    print('Este é o código do if')
+else:
+    print('Este é o else do primeiro if')
+
+print('Fora do if')    
